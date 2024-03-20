c2e269f6684fd2a6303672a77159a09a67358e0f
kamboj7791 <kambojkunal01@gmail.com>
2024-03-21 00:47:10 +0530

    logout button with backend

--- authentication/src/app/profile/page.tsx
+++ authentication/src/app/profile/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+
+      router.push("/login");
+    } catch (error: any) {
+      toast.success("logout successfully");
+      console.log(error.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center py-2 min-h-screen">
       <h1>profile</h1>
       <hr />
       <h1>profile page</h1>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 font-bold rounded">
+        Logout
+      </button>
     </div>
   );
 }
