2c77bfcddea4f7b26cb84c2bd346a79e37c3c4c0
kamboj7791 <kambojkunal01@gmail.com>
2024-04-03 01:16:16 +0530

    mailer

---
+++ authentication/src/helpers/mailer.ts
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+import User from "@/models/userModels";
+import bcryptjs from "bcryptjs";
+export const sendEmail = async ({ email, emailtype, userId }: any) => {
+  try {
+    //create a hash token
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+    if (emailtype === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailtype === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgetPasswordToken: hashedToken,
+        forgetPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
+    const mailOptions = {
+      from: "kambojkunal82@gmail.com",
+      to: email,
+      subject:
+        emailtype === "VERIFY" ? "verify your email" : "reset your password",
+    };
+  } catch (error: any) {
+    // console.log(error.message);
+    throw new Error(error.message);
+  }
+};
